a679060ed2ff4f953656e46d6da776f32083158e
Isaac Lamb <31518729+Ikegorgon@users.noreply.github.com>
2025-01-27 15:13:04 -0700

    Ticket 3 - Created User Profile Card & Styled it

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -1,22 +1,31 @@
-/**
- * TODO: Ticket 3:
- * Implement authentication using Auth0:
- * - Get the user data from Auth0
- * - Create and style the component
- * - Display the data
- * - Make this page a protected Route
- */
-const Profile = () => {
-  // TODO: Replace these with functionality from Auth0
-  const isLoading = false;
-  const user = true;
+import { useAuth0 } from "@auth0/auth0-react";
 
-  if (isLoading || !user) {
+const Profile = () => {
+  // Deconstruct User and Error variables from useAuth0
+  const { isLoading, error, user, logout } = useAuth0();
+
+  if (isLoading && !user) {
     return <div className='text-center p-4'>Loading...</div>;
   }
+  if (error) {
+    return <div className='text-center p-4'>Error...</div>
+  }
 
+  // Return Profile Card
   return (
-    <div>Profile Page</div>
+    <section>
+      <div className='flex-c'>
+        <div className='flex justify-center m-14 gap-20'>
+          <div className='profile-display flex-c gap-3'>
+            <img src={user.picture} alt='Profile Picture' />
+            <h3>{user.nickname}</h3>
+            <hr />
+            <a href={'mailto:' + user.email}>{user.email}</a>
+            <button onClick={logout}>Logout</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
