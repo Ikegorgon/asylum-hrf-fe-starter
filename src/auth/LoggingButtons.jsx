5fbe1e5a2b7806950109d5811c9ef3855d8eb936
Isaac Lamb <31518729+Ikegorgon@users.noreply.github.com>
2025-01-27 15:12:23 -0700

    Ticket 3 - Implemented Logging Buttons & Functionality

--- src/auth/LoggingButtons.jsx
+++ src/auth/LoggingButtons.jsx
@@ -1,24 +1,24 @@
-/**
- * TODO: Ticket 3:
- * Implement authentication and logging functionality using Auth0
- */
+import { useAuth0 } from "@auth0/auth0-react";
+
 export const LoggingButtons = () => {
-  // TODO: Replace these with Auth0 functionality
-  const isAuthenticated = false;
+  // Deconstruct logging methods from useAuth0
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
   const buttonText = isAuthenticated ? 'Log Out' : 'Log In';
 
   const handleLogging = () => {
     if (isAuthenticated) {
-      // TODO: Add Logout functionality here:
+      // Logout Method
+      logout({logoutParams: {returnTo: window.location.origin}});
     } else {
-      // TODO: Add Redirect functionality here:
+      // Login with Redirect Method
+      loginWithRedirect();
     }
   };
 
   return (
-    <button className='nav-btn  px-4 py-1' onClick={handleLogging}>
-      {buttonText}
-    </button>
+      <button className='nav-btn  px-4 py-1' onClick={handleLogging}>
+        {buttonText}
+      </button>
   );
 };
